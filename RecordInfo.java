8a310308c6d66076195c1de1eba053e30880fcb7
Christian Benjaminsson <christiansdator@Christians-MacBook-Pro.local>
2019-09-24 20:19:02 +0200

    Push file RecordInfo.java

--- RecordInfo.java
+++ RecordInfo.java
@@ -5,11 +5,20 @@ public class RecordInfo {
     private String recordName;
     private String artist;
 
-    public record(String recordName, String artist)
+    public RecordInfo(String recordName, String artist) {
         this.recordName = recordName;
         this.artist = artist;
+    }
 
     public static RecordInfo createRecordinfo(String recordName, String artist) {
         return new RecordInfo(recordName, artist);
+    }
+
+    public String getRecordName() {
+        return recordName;
+    }
+
+    public String getArtist() {
+        return artist;
     }
 }
